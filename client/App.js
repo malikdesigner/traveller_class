62112a238ec8714d4fbf815a50bc5295a7bd233b
Naveed <naveed@mcpinsight.com>
2023-11-26 23:00:10 +0500

    app.js

--- client/App.js
+++ client/App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import UserProfile from './components/userProfile'
+import Login from './components/Login';
+import MainPage from './components/MainPage';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        {/* Add more screens as needed */}
+        <Stack.Screen name="UserProfile" component={UserProfile} />
+        <Stack.Screen name="MainPage" component={MainPage} />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f0f0f0', // Grey background color
+
   },
 });
